4c88f86817386d09f658be22e705af0d64b32fb0
jazzari <passionjazz@gmail.com>
2019-01-03 12:16:56 -0300

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,9 +15,11 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  
-  # include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
 
   # Add more helper methods to be used by all tests here...
